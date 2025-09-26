905013560646009ed9691603ad6f69f0ed4da246
AkankshaRaj07 <162539893+AkankshaRaj07@users.noreply.github.com>
2025-09-26 21:29:51 +0530

    Time: 0 ms (100%), Space: 41.2 MB (35.48%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,15 +2,13 @@ class Solution {
     public int rob(int[] nums) {
         int n= nums.length;
         int dp [] = new int[n+1];
-        Arrays.fill(dp,-1);
-        return recur(nums,n-1,dp);
-    }
-    public int recur(int [] nums, int n,int [] dp){
-        if(n==0) return nums[0];
-        if(n==-1) return 0;
-        if(dp[n]!=-1) return dp[n];
-        int pick= nums[n] + recur(nums,n-2,dp);
-        int nopick= recur(nums,n-1,dp);
-        return dp[n]=Math.max(pick,nopick);
+        dp[0]=0;
+        dp[1]=nums[0];
+        for(int i=2; i<=n; i++){
+            int pick= nums[i-1] + dp[i-2];
+            int nopick= dp[i-1];
+            dp[i]=Math.max(pick,nopick);
+        }
+        return dp[n];
     }
 }
