862ded0cad32336be5a0cd22553b444da5d60454
AkankshaRaj07 <162539893+AkankshaRaj07@users.noreply.github.com>
2025-09-26 22:30:12 +0530

    Time: 0 ms (100%), Space: 41 MB (65.13%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,19 +1,42 @@
 class Solution {
-    public int robLinear(int[] nums,int start,int end) {
-        int money = 0;              // max money if we robbed i-2
-        int maxMoney =0;     // max money till i-1
-
-        for (int i = start; i<= end; i++) {
-            int curr = Math.max(money + nums[i], maxMoney);
-            money = maxMoney;
-            maxMoney = curr;
+    public int rob1(int[] nums) {
+        int n= nums.length;
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        int prev2=0;
+        int prev1=nums[0];
+        int ans=0;
+        for(int i=1; i<n;i++){
+            int pick=nums[i]+prev2;
+            int nopick=prev1;
+            ans=Math.max(pick,nopick);
+            prev2=prev1;
+            prev1=ans;
         }
-
-        return maxMoney;
+        return ans;
     }
-    public int rob(int[] nums){
-        if (nums.length == 0) return 0;
-        if (nums.length == 1) return nums[0];
-        return Math.max(robLinear(nums,0,nums.length-2),robLinear(nums,1,nums.length-1));
+    public int rob(int [] nums){
+        int n=nums.length;
+        if(n==1) return nums[0];
+        int nums1[]= new int[n-1];
+        int nums2[]= new int [n-1];
+        for(int i = 1, j = 0; i < n; i++, j++){
+            nums1[j] = nums[i];
+        }
+        for(int i = 0, k = 0; i < n-1; i++, k++){
+            nums2[k] = nums[i];
+        }
+        // int j=0,k=0;
+        // for(int i=1; i<n; i++){
+        //     if(i!=0){
+        //         nums1[j]=nums[i];
+        //         j++;
+        //     }
+        //     if(i!=n-1){
+        //         nums2[k]=nums[i];
+        //         k++;
+        //     }
+        // }
+        return Math.max(rob1(nums1),rob1(nums2));
     }
 }
