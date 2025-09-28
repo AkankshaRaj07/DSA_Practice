66f43cc03e4421b23becae9b217df0b659e536fa
AkankshaRaj07 <162539893+AkankshaRaj07@users.noreply.github.com>
2025-09-28 16:34:52 +0530

    Time: 14 ms (87.91%), Space: 45.3 MB (25.97%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,26 +1,31 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int dp[][] = new int[n][amount + 1];
+        //int dp[][] = new int[n][amount + 1];
+        int [] prev = new int [amount+1];
+        prev[0]=0;
         for (int a = 0; a <= amount; a++) {
                 if ((a % coins[0]) == 0) {
-                    dp[0][a] = a / coins[0];
+                    prev[a] = a / coins[0];
                 }
-                else dp[0][a] = (int) 1e9;
+                else prev[a] = (int) 1e9;
         }
         for (int i = 1; i < n; i++) {
+            int [] cur= new int [amount+1];
+            cur[0]=0;
             for (int a = 0; a <= amount; a++) {
                 int pick = (int) (1e9);
                 if (a>= coins[i]) {
-                    pick = 1 + dp[i][a - coins[i]];
+                    pick = 1 + cur[a - coins[i]];
                 }
-                int noPick = dp[i - 1][a];
-                dp[i][a] = Math.min(pick, noPick);
+                int noPick = prev[a];
+                cur[a] = Math.min(pick, noPick);
             }
+            prev=cur;
         }
-        if (dp[n-1][amount] == (int) (1e9)){
-            dp[n-1][amount] = -1;
+        if (prev[amount] == (int) (1e9)){
+            prev[amount] = -1;
         }
-        return dp[n-1][amount];
+        return prev[amount];
     }
 }
