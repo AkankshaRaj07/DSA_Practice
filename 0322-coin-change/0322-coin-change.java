b0562e46506bc3109e9343561fdf036a76e4ceed
AkankshaRaj07 <162539893+AkankshaRaj07@users.noreply.github.com>
2025-09-28 16:30:20 +0530

    Time: 15 ms (77.88%), Space: 45 MB (34.8%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,29 +1,26 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int n= coins.length;
-        int dp [] [] = new int [n][amount+1];
-        for(int i=0; i<n; i++){
-            Arrays.fill(dp[i],-1);  
+        int n = coins.length;
+        int dp[][] = new int[n][amount + 1];
+        for (int a = 0; a <= amount; a++) {
+                if ((a % coins[0]) == 0) {
+                    dp[0][a] = a / coins[0];
+                }
+                else dp[0][a] = (int) 1e9;
         }
-        int res= rec(coins, amount, n-1,dp);
-        if(res==(int)(1e9)) res=-1;
-        return res;
-
-    }
-    public int rec(int [] coins, int amount, int n, int [][] dp){
-        if(n==0){
-            if((amount % coins[n] )==0){
-                return dp[n][amount] = amount/coins[n];
+        for (int i = 1; i < n; i++) {
+            for (int a = 0; a <= amount; a++) {
+                int pick = (int) (1e9);
+                if (a>= coins[i]) {
+                    pick = 1 + dp[i][a - coins[i]];
+                }
+                int noPick = dp[i - 1][a];
+                dp[i][a] = Math.min(pick, noPick);
             }
-            return dp[n][amount] = (int) 1e9;
         }
-        if(dp[n][amount] != -1) return dp[n][amount];
-        int pick = (int) (1e9);
-        if(coins[n] <= amount){
-            pick= 1+ rec(coins, amount- coins[n], n,dp);
+        if (dp[n-1][amount] == (int) (1e9)){
+            dp[n-1][amount] = -1;
         }
-        int noPick= rec(coins, amount, n-1,dp);
-        return dp[n][amount] = Math.min(pick,noPick);
-
+        return dp[n-1][amount];
     }
 }
