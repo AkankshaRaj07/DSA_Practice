9657b4fe52dbe4c5364e3eb5371ed9b8d20f39dc
AkankshaRaj07 <162539893+AkankshaRaj07@users.noreply.github.com>
2025-10-12 17:18:32 +0530

    Time: 0 ms (100%), Space: 41.8 MB (93.46%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -5,15 +5,39 @@ class Solution {
     // }
 
     //Better Approach
-    public void sortColors(int[] nums){
-        int count0=0;
-        int count1=0;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] == 0) count0++;
-            else if(nums[i] == 1) count1++;
+    // public void sortColors(int[] nums){
+    //     int count0=0;
+    //     int count1=0;
+    //     for(int i=0; i<nums.length; i++){
+    //         if(nums[i] == 0) count0++;
+    //         else if(nums[i] == 1) count1++;
+    //     }
+    //     for(int i=0; i<count0; i++ ) nums[i]=0;
+    //     for(int i=count0; i<count1+count0; i++ ) nums[i]=1;
+    //     for(int i=count1+count0; i<nums.length; i++ ) nums[i]=2;
+    // }
+
+    //Optimal Approach
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+        while (mid<=high) {
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                mid++;
+                low++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                int temp = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp;
+                high--;
+            }
         }
-        for(int i=0; i<count0; i++ ) nums[i]=0;
-        for(int i=count0; i<count1+count0; i++ ) nums[i]=1;
-        for(int i=count1+count0; i<nums.length; i++ ) nums[i]=2;
     }
+
 }
