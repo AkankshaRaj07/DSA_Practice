3cfb19ec87d6985b50bc7bb3ad2fffbad532aae9
Akanksha Kumari <162539893+AkankshaRaj07@users.noreply.github.com>
2026-02-04 22:54:28 +0530

    Time: 18 ms (15.66%), Space: 49 MB (66.66%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,38 +1,39 @@
 class Solution {
     class Pair {
-    int r;
-    int c;
-    Pair(int r, int c) {
-        this.r = r;
-        this.c = c;
-    }
+        int r;
+        int c;
+
+        Pair(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
     }
 
     public int[][] updateMatrix(int[][] mat) {
-        int m=mat.length;
-        int n=mat[0].length;
-        int [][] result = new int[m][n];
-        Queue<Pair> q= new LinkedList<>();
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                 if(mat[i][j]==0) {
-                    result[i][j]=0;
+        int m = mat.length;
+        int n = mat[0].length;
+        int[][] result = new int[m][n];
+        Queue<Pair> q = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    result[i][j] = 0;
                     q.offer(new Pair(i, j));
-                 }
-                 else result[i][j]=-1;
+                } else
+                    result[i][j] = -1;
             }
         }
-        while(!q.isEmpty()){
-            Pair curr= q.poll();
+        while (!q.isEmpty()) {
+            Pair curr = q.poll();
             int r = curr.r;
             int c = curr.c;
-            int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
-            for(int i=0; i<4; i++){
-                int nr=r+ dir[i][0];
-                int nc=c+ dir[i][1];
-                if(nr>=0 && nc>=0 && nr<m && nc<n && result[nr][nc]==-1){
-                    result[nr][nc]=result[r][c]+1;
-                    q.offer(new Pair(nr,nc));
+            int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+            for (int i = 0; i < 4; i++) {
+                int nr = r + dir[i][0];
+                int nc = c + dir[i][1];
+                if (nr >= 0 && nc >= 0 && nr < m && nc < n && result[nr][nc] == -1) {
+                    result[nr][nc] = result[r][c] + 1;
+                    q.offer(new Pair(nr, nc));
                 }
             }
         }
