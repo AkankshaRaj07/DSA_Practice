790b074fc5e7b8c7746240f5eafab7695dd624f3
Akanksha Kumari <162539893+AkankshaRaj07@users.noreply.github.com>
2026-02-24 17:02:26 +0530

    Time: 0 ms (100%), Space: 43.8 MB (57.04%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
@@ -19,7 +19,7 @@ class Solution {
     }
     private int dfs(TreeNode node, int curr) {
         if (node == null) return 0;
-        curr = (curr << 1) | node.val;
+        curr = curr * 2 + node.val;
         if (node.left == null && node.right == null) return curr;
         return dfs(node.left, curr) + dfs(node.right, curr);
     }
