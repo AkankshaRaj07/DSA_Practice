1019cf1a31e2745cc3b5c05af64e1f678d45e648
Akanksha Kumari <162539893+AkankshaRaj07@users.noreply.github.com>
2026-03-12 11:42:52 +0530

    Time: 5 ms (63.25%), Space: 75.3 MB (20.2%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.java
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.java
@@ -3,14 +3,13 @@ class Solution {
         int n = nums.length;
         int flips = 0;
         int flipCountFromPastFori = 0;
-        boolean [] isFlipped = new boolean [n];
         for(int i =0; i<n ; i++){
-            if(i>=k && isFlipped[i-k]) flipCountFromPastFori--;
+            if(i>=k && nums[i-k]==2) flipCountFromPastFori--;
             if(flipCountFromPastFori % 2 == nums[i]){
                 if(i + k > n) return -1;
                 flipCountFromPastFori++;
                 flips++;
-                isFlipped[i] = true;
+                nums[i]=2;
             }
         }
         return flips;
