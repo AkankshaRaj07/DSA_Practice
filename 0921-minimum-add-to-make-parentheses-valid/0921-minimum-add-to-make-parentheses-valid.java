5faaf7afab1e617f184b814cab76bc7280ab05ba
Akanksha Kumari <162539893+AkankshaRaj07@users.noreply.github.com>
2026-03-25 10:53:05 +0530

    Time: 0 ms (100%), Space: 43.1 MB (31.68%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -1,17 +1,13 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        Stack <Character> st = new Stack<>();
-        for(char c: s.toCharArray()){
-            if(c==')' && !st.isEmpty() && st.peek()=='(') st.pop();
-            else st.push(c);
+        int open = 0, add = 0;
+        for(char c : s.toCharArray()){
+            if(c == '(') open++;
+            else {
+                if(open > 0) open--;
+                else add++;
+            }
         }
-        int open = 0, close = 0;
-        while(!st.isEmpty()){
-            char val = st.pop();
-            if(val=='(') open++;
-            else close++;
-        }
-        int ans = Math.abs(open+close);
-        return ans;
+        return open + add;
     }
 }
