20d550276687158e23c59b2da2ba0c58679a23da
Akanksha Kumari <162539893+AkankshaRaj07@users.noreply.github.com>
2026-05-29 23:11:55 +0530

    Time: 0 ms (100%), Space: 43 MB (18.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,7 +1,6 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n + 1];
         int prev = 0;
         int curr = nums[0];
         for (int i = 2; i <= n; i++) {
