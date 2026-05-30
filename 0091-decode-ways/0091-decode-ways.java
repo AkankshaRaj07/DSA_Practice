b6bb39bbea496f60ae6ed4bcd382e657fa7a5eef
Akanksha Kumari <162539893+AkankshaRaj07@users.noreply.github.com>
2026-05-30 09:45:49 +0530

    Time: 0 ms (100%), Space: 43.1 MB (48.82%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -13,7 +13,7 @@ class Solution {
         if(dp[i]!=-1) return dp[i];
         int check = 0;
         if (i + 1 < s.length()) {
-            int num = Integer.parseInt(s.substring(i, i + 2));
+            int num = (s.charAt(i) - '0') * 10 + (s.charAt(i + 1) - '0');
             if (num >= 10 && num <= 26) {
                 check = solve(i + 2, s,dp);
             }
