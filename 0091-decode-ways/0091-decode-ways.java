9eb9bf54e52823a1640a64d378e4425e3cc8c726
Akanksha Kumari <162539893+AkankshaRaj07@users.noreply.github.com>
2026-05-30 09:54:39 +0530

    Time: 1 ms (83.04%), Space: 43.1 MB (48.82%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,23 +1,21 @@
 class Solution {
     public int numDecodings(String s) {
         int n = s.length();
-        int [] dp = new int [n+1];
-        Arrays.fill(dp,-1);
-        if(s.charAt(0) =='0') return 0;
-        return solve(0,s,dp);
-    }
-
-    public int solve(int i, String s, int [] dp) {
-        if (i == s.length()) return 1;
-        if (s.charAt(i) == '0') return 0;
-        if(dp[i]!=-1) return dp[i];
-        int check = 0;
-        if (i + 1 < s.length()) {
-            int num = (s.charAt(i) - '0') * 10 + (s.charAt(i + 1) - '0');
-            if (num >= 10 && num <= 26) {
-                check = solve(i + 2, s,dp);
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
+                dp[i] = 0;
+                continue;
+            }
+            dp[i] = dp[i + 1];
+            if (i + 1 < n) {
+                int num = (s.charAt(i) - '0') * 10 + (s.charAt(i + 1) - '0');
+            
+            if (num >= 10 && num <= 26)
+                dp[i] += dp[i + 2];
             }
         }
-        return dp[i] = solve(i + 1, s,dp) + check;
+        return dp[0];
     }
 }
