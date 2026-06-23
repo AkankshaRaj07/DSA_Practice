fc399c1b7dd8f75651ca2a22a2e9fde19f1baf69
Akanksha Kumari <162539893+AkankshaRaj07@users.noreply.github.com>
2026-06-24 00:17:57 +0530

    Time: 8 ms (11.24%), Space: 45.4 MB (29.06%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,21 +1,29 @@
+import java.util.*;
+import java.util.function.BiFunction;
+
 class Solution {
-    public int operate(int a, int b, String s){
-        if(s.equals("+")) return a + b;
-        else if(s.equals("-")) return a - b;
-        else if(s.equals("*")) return a * b;
-        else return a / b;
-    }
     public int evalRPN(String[] tokens) {
-        Stack<Integer>  st = new Stack<>();
-        for(String s: tokens){
-            if(s.equals("+") || s.equals("-") ||
-               s.equals("*") || s.equals("/")){
+
+        Map<String, BiFunction<Integer, Integer, Integer>> ops = new HashMap<>();
+
+        ops.put("+", (a, b) -> a + b);
+        ops.put("-", (a, b) -> a - b);
+        ops.put("*", (a, b) -> a * b);
+        ops.put("/", (a, b) -> a / b);
+
+        Stack<Integer> st = new Stack<>();
+
+        for (String s : tokens) {
+            if (ops.containsKey(s)) {
                 int b = st.pop();
                 int a = st.pop();
-                int result = operate(a,b,s);
-                st.push(result);
-            }else st.push(Integer.parseInt(s));
+
+                st.push(ops.get(s).apply(a, b));
+            } else {
+                st.push(Integer.parseInt(s));
+            }
         }
+
         return st.peek();
     }
 }
