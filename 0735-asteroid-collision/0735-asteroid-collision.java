ea5b594dbdab572a8d138f6796f7d556ec21365e
Akanksha Kumari <162539893+AkankshaRaj07@users.noreply.github.com>
2026-06-24 15:13:41 +0530

    Time: 4 ms (83.04%), Space: 46.8 MB (82.55%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -4,24 +4,22 @@ class Solution {
         for (int i : asteroids) {
             while (!st.isEmpty() && i < 0 && st.peek() > 0) {
                 int sum = st.peek() + i;
-                if (sum > 0)
-                    i = 0;
-                else if (sum < 0)
-                    st.pop();
+                if (sum > 0) i = 0;
+                else if (sum < 0) st.pop();
                 else {
                     st.pop();
                     i = 0;
                 }
             }
-            if (i != 0)
-                st.push(i);
+            if (i != 0) st.push(i);
         }
-        ArrayList<Integer> ans = new ArrayList<>();
-    while(!st.isEmpty())ans.add(st.pop());
-    int[] result = new int[ans.size()];
-    Collections.reverse(ans);
-    for(int i = 0;i<ans.size();i++) result[i] = ans.get(i);
-    return result;
+        int[] result = new int[st.size()];
+        int n = st.size();
+        while(!st.isEmpty()){
+            result[n-1] = (st.pop());
+            n--;
+        }
+        return result;
     }
     
 }
