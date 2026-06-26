4aadcfff509f1d76203930a37d165595269b48fc
Akanksha Kumari <162539893+AkankshaRaj07@users.noreply.github.com>
2026-06-27 00:36:02 +0530

    Time: 25 ms (13.43%), Space: 47.4 MB (6.8%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.java
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.java
@@ -1,23 +1,27 @@
 class Solution {
     public String reverseParentheses(String s) {
-        Stack<Integer> st = new Stack<>();
-        String res = "";
-        int br =0;
+        Stack<Character> st = new Stack<>();
         for(int i = 0; i<s.length() ; i++){
             char c = s.charAt(i);
-            if(c == '(') st.push(res.length()); 
-            else if(c>='a' && c<='z') res+=c;
+            StringBuilder temp = new StringBuilder();
+            if(c == '(') st.push(c); 
+            else if(Character.isLetter(c)) st.push(c);
             else if(c==')'){
-                int start = st.pop();
-                res = reverse(res, start);
+                while(!st.isEmpty() && st.peek()!='('){
+                    temp.append(st.pop());
+                }
+                st.pop();
+                for(char a: temp.toString().toCharArray()) st.push(a);
             }
         }
-        return res;
+        StringBuilder ans = new StringBuilder();
+        while(!st.isEmpty()) ans.append(st.pop());
+        return ans.reverse().toString();
     }
-    public String reverse(String res, int start){
-        String first = res.substring(0, start);
-        String second = new StringBuilder(res.substring(start)).reverse().  toString();
-        return first + second;
+    // public String reverse(String res, int start){
+    //     String first = res.substring(0, start);
+    //     String second = new StringBuilder(res.substring(start)).reverse().  toString();
+    //     return first + second;
         
-    }
+    // }
 }
