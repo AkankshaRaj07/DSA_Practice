1649723eeb7230bbde58dd9d6601ce9fe365a601
Akanksha Kumari <162539893+AkankshaRaj07@users.noreply.github.com>
2026-06-26 18:56:07 +0530

    Time: 8 ms (96.79%), Space: 46.3 MB (95.36%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.java
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -1,25 +1,50 @@
 class Solution {
     public String removeKdigits(String num, int k) {
-        Stack<Character> st= new Stack<>();
-        for(char dig: num.toCharArray()){
-            while(!st.isEmpty() && k>0 && st.peek() > dig){
-                st.pop();
+        if (num.length() == k)
+            return "0";
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < num.length(); i++) {
+            char c = num.charAt(i);
+            while (res.length() > 0 && k > 0 && res.charAt(res.length() - 1) > c) {
+                res.deleteCharAt(res.length() - 1);
                 k--;
             }
-            st.push(dig);
+            res.append(c);
         }
-        while(k>0 && !st.isEmpty()){
-            st.pop();
+        while (k > 0) {
+            res.deleteCharAt(res.length() - 1);
             k--;
         }
-        StringBuilder sb= new StringBuilder();
-        while(!st.isEmpty()){
-            sb.append(st.pop());
+
+        // Remove leading zeros
+        while (res.length() > 0 && res.charAt(0) == '0') {
+            res.deleteCharAt(0);
         }
-        sb.reverse();
-        while(sb.length() > 0 && sb.charAt(0) == '0'){
-            sb.deleteCharAt(0);
-        }
-        return sb.length() > 0 ? sb.toString() : "0";
+
+        return res.length() == 0 ? "0" : res.toString();
+
     }
+    // public String removeKdigits(String num, int k) {
+    //     Stack<Character> st= new Stack<>();
+    //     for(char dig: num.toCharArray()){
+    //         while(!st.isEmpty() && k>0 && st.peek() > dig){
+    //             st.pop();
+    //             k--;
+    //         }
+    //         st.push(dig);
+    //     }
+    //     while(k>0 && !st.isEmpty()){
+    //         st.pop();
+    //         k--;
+    //     }
+    //     StringBuilder sb= new StringBuilder();
+    //     while(!st.isEmpty()){
+    //         sb.append(st.pop());
+    //     }
+    //     sb.reverse();
+    //     while(sb.length() > 0 && sb.charAt(0) == '0'){
+    //         sb.deleteCharAt(0);
+    //     }
+    //     return sb.length() > 0 ? sb.toString() : "0";
+    // }
 }
