10221523f66035d72e6276527e8cd410d3fa0bee
Akanksha Kumari <162539893+AkankshaRaj07@users.noreply.github.com>
2026-08-16 15:07:39 +0530

    Time: 9 ms (60.69%), Space: 46.4 MB (65.61%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxLength(List<String> arr) {
+        String temp = "";
+        int n = arr.size();
+        return solve(arr,0,n,temp);
+    }
+    private int solve(List<String> arr, int i , int n, String temp){
+        if(i>=n) return temp.length();
+        int pick = 0, noPick = 0;
+        if(hasDuplicate(temp, arr.get(i))){
+            noPick = solve(arr,i+1,n,temp);
+        }else{
+            pick = solve(arr,i+1,n,temp+arr.get(i));
+            noPick = solve(arr,i+1,n,temp);
+        }
+        return Math.max(pick, noPick);
+    }
+    private boolean hasDuplicate(String s1, String s2){
+        int[] freq = new int[26];
+        for(char a: s1.toCharArray()){
+            if(freq[a-'a']>0) return true;
+            freq[a-'a']++;
+        }
+        for(char a: s2.toCharArray()){
+            if(freq[a-'a']>0) return true;
+            freq[a-'a']++;
+        }
+        return false;
+
+    }
+}
