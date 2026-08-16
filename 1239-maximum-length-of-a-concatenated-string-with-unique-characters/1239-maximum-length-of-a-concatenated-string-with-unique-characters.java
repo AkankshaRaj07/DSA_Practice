12dd13615cca97b78b27c337345437831c0b95e8
Akanksha Kumari <162539893+AkankshaRaj07@users.noreply.github.com>
2026-08-16 15:21:17 +0530

    Time: 30 ms (15.32%), Space: 53.3 MB (5.2%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,29 +1,46 @@
 class Solution {
-    public int maxLength(List<String> arr) {
-        String temp = "";
-        int n = arr.size();
-        return solve(arr,0,n,temp);
-    }
-    private int solve(List<String> arr, int i , int n, String temp){
-        if(i>=n) return temp.length();
-        int pick = 0, noPick = 0;
-        if(!hasDuplicate(temp, arr.get(i))){
-             pick = solve(arr,i+1,n,temp+arr.get(i));
-        }
-        noPick = solve(arr,i+1,n,temp);
-        return Math.max(pick, noPick);
-    }
-    private boolean hasDuplicate(String s1, String s2){
-        int[] freq = new int[26];
-        for(char a: s1.toCharArray()){
-            if(freq[a-'a']>0) return true;
-            freq[a-'a']++;
-        }
-        for(char a: s2.toCharArray()){
-            if(freq[a-'a']>0) return true;
-            freq[a-'a']++;
-        }
-        return false;
+    Map<String, Integer> dp = new HashMap<>();
 
+    public int maxLength(List<String> arr) {
+        return solve(arr, 0, "");
+    }
+
+    private int solve(List<String> arr, int i, String temp) {
+        if(i >= arr.size())
+            return temp.length();
+
+        String key = i + "#" + temp;
+
+        if(dp.containsKey(key))
+            return dp.get(key);
+
+        int noPick = solve(arr, i + 1, temp);
+        int pick = 0;
+
+        if(!hasDuplicate(temp, arr.get(i))) {
+            pick = solve(arr, i + 1, temp + arr.get(i));
+        }
+
+        dp.put(key, Math.max(pick, noPick));
+
+        return dp.get(key);
+    }
+
+    private boolean hasDuplicate(String s1, String s2) {
+        int[] freq = new int[26];
+
+        for(char c : s1.toCharArray()) {
+            if(freq[c - 'a'] > 0)
+                return true;
+            freq[c - 'a']++;
+        }
+
+        for(char c : s2.toCharArray()) {
+            if(freq[c - 'a'] > 0)
+                return true;
+            freq[c - 'a']++;
+        }
+
+        return false;
     }
 }
